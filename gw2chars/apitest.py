507a61d16a24ed204d84f865d1529b687599f8f1
Martin P <martin@hepau.dk>
2018-03-19 15:28:41 +0100

    Bleh

--- gw2chars/apitest.py
+++
@@ -1,5 +0,0 @@
-import os
-import sqlite3
-import requests
-import json
-
--- gw2chars/gw2chars.py
+++ gw2chars/gw2chars.py
@@ -32,7 +32,7 @@ def close_db(error):
 
 def init_db():
     db = get_db()
-    with app.open_resource('db-scripts.sql', mode='r') as f:
+    with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
 
